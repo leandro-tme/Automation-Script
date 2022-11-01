aa6f2ef9138a37e5fbdc480262c05ab4da4b7fed
Leandro Taipe <leandro.taipe@thedreamcorporation.com>
2022-11-01 22:20:41 +0000

    First commit/ query if salt state running

---
+++ Automation.py
@@ -0,0 +1,36 @@
+#import salt 
+import csv
+
+#To execute scripts via bundled Python runtime, 
+# either run the script with /path/to/salt python script.py or use #!/path/to/salt python shebang
+
+
+
+
+def check_viveconsole_settings():
+   
+   
+   with open("venue_pcs.txt") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Pc names are {", ".join(row)}')
+            line_count += 1
+        else:
+            #query salt for running jobs csv
+            #client.cmd(row[0] , "saltutil.running")
+            #client.cmd(row[1] , "saltutil.running")
+            #client.cmd(row[2] , "saltutil.running")
+            line_count += 1
+    
+    print(f'Processed {line_count} lines.')
+            
+
+         
+    
+check_viveconsole_settings()   
+    
+    
+     
+
