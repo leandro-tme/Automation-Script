a849534dcdfdbab66e615ea44a4674dad3a1a034
Taiwai98 <dtwong98@gmail.com>
2022-11-03 20:26:17 +0000

    first commit

--- tdc_daily_checklist.py
+++ tdc_daily_checklist.py
@@ -24,7 +24,7 @@ def check_viveconsole_settings():
             #client.cmd(row[1] , "saltutil.running")
             #client.cmd(row[2] , "saltutil.running")
             line_count += 1
-    
+            #hello
     print(f'Processed {line_count} lines.')
             
 
