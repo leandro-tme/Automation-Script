0f0f2829f86e7d5b007c973e439efc7bef6f020d
Leandro Taipe <leandro.taipe@thedreamcorporation.com>
2022-12-05 11:19:00 +0000

    general class layout

--- tdc_daily_checklist.py
+++ tdc_daily_checklist.py
@@ -1,37 +1,30 @@
-#import salt 
-import csv
-#import salt.utils.http
+class daily_checklist:
+    def execution():
+        return
 
-#To execute scripts via bundled Python runtime, 
-# either run the script with /path/to/salt python script.py or use #!/path/to/salt python shebang
-
-
-
-# communicate with salt to obtain correct file from salt master and compare minion file to file on database
-
-def check_viveconsole_settings():
-   
-   
-   with open("venue_pcs.txt") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Pc names are {", ".join(row)}')
-            line_count += 1
-        else:
-            #query salt for running jobs csv
-            #client.cmd(row[0] , "saltutil.running")
-            #client.cmd(row[1] , "saltutil.running")
-            #client.cmd(row[2] , "saltutil.running")
-            line_count += 1
+    def data_fetch():
+        return 
     
-    print(f'Processed {line_count} lines.')
-            
-
-         
+    def highstate_check():
+        return
     
-check_viveconsole_settings()   
+    def hyperx_check():
+        return
+    
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
     
     
      
