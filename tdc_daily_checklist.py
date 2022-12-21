685961a67a5a195875853a99e5f410fbfa1d0578
Leandro Taipe <leandro.taipe@thedreamcorporation.com>
2022-12-21 20:57:36 +0000

    Method that returns list of mismatching keys

--- tdc_daily_checklist.py
+++ tdc_daily_checklist.py
@@ -1,22 +1,43 @@
-class daily_checklist:
-    def execution():
-        return
 
-    def data_fetch():
-        return 
+import requests
+import pepper.script 
+
+import json
+
+#request = requests.get('https://localhost:8000/jobs')
+
+def generate_pc_list():
+    with open("data.json") as json_file:
+        s1 = json.load(json_file)
+
+    with open("venue_pcs.json") as json_file:
+        s2 = json.load(json_file)
+
+    keys = {}
+    mismatching_pc = []
+    for dictionary in s1:
+        keys[dictionary["name"]] = dictionary["name"]
     
-    def highstate_check():
+    for dictionary in s2:
+        if dictionary["name"] in keys:
+            if keys[dictionary["name"]] != dictionary["name"]:
+                print("Mismatching Keys Found!")
+                mismatching_pc.append(dictionary)
+            else:
+                print("this keys matches.")
+        else:
+            print("Dictionary not found in the first JSON file")
+            mismatching_pc.append(dictionary)
+        
+    return mismatching_pc
+
+returned_list = generate_pc_list()
+print(returned_list)
+def highstate_check():
         return
     
-    def hyperx_check():
+def hyperx_check():
         return
-    
-
-
-
-   
-
-
 
 
 
