5d9b361269fbfd7e7ab7460822251e92ec2bf017
Leandro Taipe <leandro.taipe@thedreamcorporation.com>
2023-01-04 16:49:50 +0000

    code to create empty table sqlite

--- tdc_daily_checklist.py
+++ tdc_daily_checklist.py
@@ -1,10 +1,28 @@
 
 import requests
 import pepper.script 
-
 import json
+import sqlite3
+
+#connect to database
+conn  = sqlite3.connect('mismatchingPcs.db')
+#create cursor
+c = conn.cursor()
+
+# create a table 
+c.execute("""CREATE TABLE mismatchingPCs (
+        pc_name TEXT,
+        number_of_occurrences INTEGER
+    )""")
+
+#commit our command 
+conn.commit()
+# close the connection
+conn.close()
+
 
 #request = requests.get('https://localhost:8000/jobs')
+
 
 def generate_pc_list():
     with open("data.json") as json_file:
@@ -28,10 +46,12 @@ def generate_pc_list():
         else:
             print("Dictionary not found in the first JSON file")
             mismatching_pc.append(dictionary)
+    
         
     return mismatching_pc
 
 returned_list = generate_pc_list()
+
 print(returned_list)
 def highstate_check():
         return
